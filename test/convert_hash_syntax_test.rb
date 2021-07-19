38b02d1799c092219f12eb26bdb07febf3087073
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-19 12:50:12 +0900

    convertテストが通った

--- test/convert_hash_syntax_test.rb
+++ test/convert_hash_syntax_test.rb
@@ -3,7 +3,17 @@ require './lib/convert_hash_syntax'
 
 class ConvertHashSyntaxTest < Minitest::Test
   def test_convert_hash_syntax
-    assert_equal '{}', convert_hash_syntax('{}')
+    old_syntax = <<~TEXT
+    {
+      :name => 'Alice'
+    }
+    TEXT
+    expected = <<~TEXT
+    {
+      name: 'Alice'
+    }
+    TEXT
+    assert_equal expected, convert_hash_syntax(old_syntax)
   end
 end
 
